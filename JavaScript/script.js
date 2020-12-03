d2dd975506137e8d88bf80d9d75d7a0d3c65006e
NikolaIlic99 <68603904+NikolaIlic99@users.noreply.github.com>
2020-12-03 18:44:57 +0100

    Add files via upload
    Napravljen nav-bar i kostur sajta

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#navbar-homepage").click(function () { 
+        location.href = "../Html/index.html";
+        
+    });
+    $("#navbar-galerija").click(function () { 
+        location.href = "../Html/galerija.html";
+        
+    });
+    $("#navbar-zivotinje").click(function () { 
+        location.href = "../Html/zivotinje.html";
+        
+    });
+    $("#navbar-karte").click(function () { 
+        location.href = "../Html/karte.html";
+        
+    });
+});
