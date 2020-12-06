228503fd2e110ca0f395ccb21169d289ed14dd8c
NikolaIlic99 <68603904+NikolaIlic99@users.noreply.github.com>
2020-12-06 18:34:55 +0100

    Uradjen homepage

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+  window.onscroll = function () {
+    stickyFunction();
+  };
+
+  var navbar = document.getElementsByClassName("navbar")[0];
+
+  // Pronalazi offset za navbar
+  var offset = navbar.offsetTop;
+
+  // Kada se skrolom predje preko visine navbara, dodaje se sticky klasa
+  function stickyFunction() {
+    if (window.pageYOffset >= offset) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
   $("#navbar-homepage").click(function () {
     location.href = "../Html/index.html";
   });
@@ -11,8 +29,25 @@ $(document).ready(function () {
   $("#navbar-karte").click(function () {
     location.href = "../Html/karte.html";
   });
-  
-  var gallery = new SimpleLightbox('.gallery a', { 
-    /* options */ 
-});
+  $("#tabs").tabs();
+  $("#accordion").accordion({});
+
+  $(".popup-gallery").magnificPopup({
+    //Gallery plugin kod
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function (item) {
+        return item.el.attr("title") + "<small>Beo Zoo Vrt</small>";
+      },
+    },
+  });
 });
